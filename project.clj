5a221048bfa28888a46bfd5fe0d8d0a293c8e29b
samuel cho <samonthepiano@gmail.com>
2023-02-03 08:58:16 +0100

    0.0.4 bump some.vec

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.some/polyline "0.0.3"
+(defproject org.clojars.some/polyline "0.0.4"
   :description "Helper functions for dealing with 2D polylines. A polyline is
   just a vector of 2D (x y) vectors."
   :url "https://github.com/somecho/some.polyline"
@@ -6,7 +6,7 @@
             :url "https://opensource.org/licenses/MIT"}
 
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojars.some/vec "0.0.2"]]
+                 [org.clojars.some/vec "0.0.3"]]
   :repositories
   {"clojars" {:url "https://clojars.org/repo"
               :sign-releases false}}
